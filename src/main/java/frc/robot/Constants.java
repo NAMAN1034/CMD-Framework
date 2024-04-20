1b7fac20a37659ba5530bc2dd5851602ddb6c8b0
NAMAN1034 <155015788+NAMAN1034@users.noreply.github.com>
2024-04-20 09:45:58 -0700

    Create Constants.java

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,24 @@
+// This code is subject to the WPILib BSD license, which allows modification and sharing.
+// Refer to the license file in the root directory for details.
+
+package frc.robot;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants should be declared
+ * globally (i.e., public static). Do not put anything functional in this class.
+ *
+ * <p>It is advisable to statically import this class (or one of its inner classes) wherever the
+ * constants are needed to reduce verbosity.
+ */
+public final class Constants {
+  /**
+   * Constants related to operator inputs.
+   */
+  public static class OperatorConstants {
+    /**
+     * The port number for the driver controller.
+     */
+    public static final int kDriverControllerPort = 0;
+  }
+}
