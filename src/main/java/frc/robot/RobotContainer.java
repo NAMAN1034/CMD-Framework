1343074e6a85255fddc2e8176ac0d9ca3800eaab
NAMAN1034 <155015788+NAMAN1034@users.noreply.github.com>
2024-04-20 11:23:16 -0700

    Update RobotContainer.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,27 +1,16 @@
-// This code is subject to the WPILib BSD license, which allows modification and sharing.
-// Refer to the license file in the root directory for details.
-
 package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autonomous;
 import frc.robot.commands.ShooterWheelSpinner;
 import frc.robot.subsystems.ShooterWheelSubsystem;
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
-/**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
- * subsystems, commands, and trigger mappings) should be declared here.
- */
 public class RobotContainer {
   private final CommandXboxController m_driverController = new CommandXboxController(OperatorConstants.kDriverControllerPort);
   private final ShooterWheelSubsystem m_shooterSubsystem = new ShooterWheelSubsystem();
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     configureBindings();
   }
@@ -30,25 +19,12 @@ public class RobotContainer {
     return m_driverController;
   }
 
-  /**
-   * Use this method to define your trigger->command mappings.
-   */
   private void configureBindings() {
-    // Spin shooter wheel when X button is pressed, stop when released
     new Trigger(m_driverController.x())
-        .whenActive(new ShooterWheelSpinner(m_shooterSubsystem, 20))
-        .whenInactive(new ShooterWheelSpinner(m_shooterSubsystem, 0));
-
-    // Example of mapping a command to a button
-    // m_driverController.b().whileHeld(new ExampleCommand(m_shooterSubsystem));
+        .whileTrue(new ShooterWheelSpinner(m_shooterSubsystem, 20));
   }
 
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
   public Command getAutonomousCommand() {
-    return Autonomous.exampleAutonomous(m_shooterSubsystem);
+    return Autonomous.exampleAuto(m_shooterSubsystem);
   }
 }
