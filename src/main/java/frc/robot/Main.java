f38029ece94d66dcd0d02402c081fa03b7a29a8a
NAMAN1034 <155015788+NAMAN1034@users.noreply.github.com>
2024-04-20 09:46:25 -0700

    Create Main.java

---
+++ src/main/java/frc/robot/Main.java
@@ -0,0 +1,24 @@
+// This code is subject to the WPILib BSD license, which allows modification and sharing.
+// Refer to the license file in the root directory for details.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.RobotBase;
+
+/**
+ * Do NOT add any static variables to this class, or any initialization at all. Unless you know what
+ * you are doing, do not modify this file except to change the parameter class to the startRobot
+ * call.
+ */
+public final class Main {
+  private Main() {}
+
+  /**
+   * Main initialization function. Do not perform any initialization here.
+   *
+   * <p>If you change your main robot class, change the parameter type.
+   */
+  public static void main(String... args) {
+    RobotBase.startRobot(Robot::new);
+  }
+}
